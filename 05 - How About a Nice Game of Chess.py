149862daba6478467a6471c2da2dff69c8ad5803
miran.tuhtan <miran.tuhtan@uniri.hr>
2016-12-05 12:22:38 +0100

    slightly faster, slightly uglier

--- 05 - How About a Nice Game of Chess.py
+++ 05 - How About a Nice Game of Chess.py
@@ -11,11 +11,12 @@ while available_positions:
     char_input = (DOOR_ID+str(i)).encode()
     md5_hex = hashlib.md5(char_input).hexdigest()
 
-    if md5_hex[:5] == 5*'0' and len(first_password) < 8:
-        first_password += md5_hex[5]
-    if md5_hex[:5] == 5*'0' and md5_hex[5] in available_positions:
-        second_password[int(md5_hex[5])] = md5_hex[6]
-        available_positions.remove(md5_hex[5])
+    if md5_hex[:5] == 5*'0':
+        if len(first_password) < 8:
+            first_password += md5_hex[5]
+        if md5_hex[5] in available_positions:
+            second_password[int(md5_hex[5])] = md5_hex[6]
+            available_positions.remove(md5_hex[5])
     i += 1
 
 print("The password for a first door is:", first_password)
