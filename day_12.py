7acce219ddad2f3eb9895f9438daadecd2ee684e
narimiran <miran.tuhtan+git@gmail.com>
2017-03-22 22:02:41 +0100

    day 12 assembunny translation
    Instead of iterating through instructions (and maybe optimizing the loops), the instructions are translated to a much faster code.

--- day_12.py
+++ day_12.py
@@ -1,37 +1,23 @@
-with open("./sources/12 - Leonardo's Monorail.txt", 'r') as infile:
-    instructions = infile.read().splitlines()
+def translate_asembunny(c_val):
+    # lines 1-9
+    a = b = 1
+    d = 26 + 7 * c_val
 
+    # lines 10-16, Fibonacci
+    for _ in range(d):
+        a, b = a+b, a
 
-def execute(c):
-    register = {'a': 0, 'b': 0, 'c': c, 'd': 0}
-    interpret = lambda val: int(val) if val.isdigit() else register[val]
-    i = 0
+    # lines 17-23
+    c = 19
+    d = 14
+    a += c * d
 
-    while i < len(instructions):
-        instr, x, *y = instructions[i].split()
-        y = y[0] if y else None
-
-        if instr == 'cpy':
-            register[y] = interpret(x)
-
-        if instr == 'inc':
-            register[x] += 1
-
-        if instr == 'dec':
-            register[x] -= 1
-
-        if instr == 'jnz':
-            if interpret(x) != 0:
-                i += int(y)
-                continue
-
-        i += 1
-    return register['a']
+    return a
 
 
 print("Well, this should be quick....")
 print("Initializing all registers with 0. Firing the code.")
-print("The end value of A register is: {}".format(execute(c=0)))
+print(f"The end value of A register is: {translate_asembunny(c_val=0)}")
 print("....")
 print("What? The starting value for C register is 1?")
-print("Then the end value of A register is: {}".format(execute(c=1)))
+print(f"Then the end value of A register is: {translate_asembunny(c_val=1)}")
